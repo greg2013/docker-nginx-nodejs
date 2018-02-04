02680e2c9a41eb1cf648a7bad2482a047727a2d0
Greg Zheng <greg.zheng@indigotiger.com>
2018-02-04 18:14:50 -0500

    Colorize the text based on environmental parameter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   //res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>${process.env.MESSAGE}</h1>`);
+  res.end(`<h1 style="color:${process.env.MSG_COLOR}">${process.env.MESSAGE}</h1>`);
   res.end('<h1>Hi There!</h1>');
 }).listen(8080);
 
